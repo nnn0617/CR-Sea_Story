445452e0131cff35a25f77b8a4a3b01d7d9d0b40
nnn0617 <natu0617k@gmail.com>
2020-10-05 15:41:43 +0900

    image降下追加

--- CR Sea Story/Assets/DOTween/test/ButtonController.cs
+++ CR Sea Story/Assets/DOTween/test/ButtonController.cs
@@ -3,14 +3,36 @@ using DG.Tweening;
 
 public class ButtonController : MonoBehaviour
 {
+    RectTransform rectTransform;
+
     void Awake()
     {
-        transform.localScale = Vector3.zero;
+        rectTransform = GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector3(300, 700, 0);
         ShowWindow();
     }
 
     void ShowWindow()
     {
-        transform.DOScale(1f, 1f).SetEase(Ease.OutBounce);
+        rectTransform.DOLocalMoveY(0f, 2f).SetEase(Ease.OutBounce);
     }
 }
+
+
+
+//using UnityEngine;
+//using DG.Tweening;
+
+//public class ButtonController : MonoBehaviour
+//{
+//    void Awake()
+//    {
+//        transform.localScale = Vector3.zero;
+//        ShowWindow();
+//    }
+
+//    void ShowWindow()
+//    {
+//        transform.DOScale(1f, 1f).SetEase(Ease.OutBounce);
+//    }
+//}
