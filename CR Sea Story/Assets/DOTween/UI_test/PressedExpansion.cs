4d29d415dbc88b2838f7e0aa273f9cbeea009dfe
nnn0617 <natu0617k@gmail.com>
2020-10-09 11:11:30 +0900

    更新

---
+++ CR Sea Story/Assets/DOTween/UI_test/PressedExpansion.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class PressedExpansion : MonoBehaviour
+{
+    void Awake()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        transform.DOScale(3.3f, 0.5f).SetEase(Ease.OutElastic);
+        GetComponentInChildren<Text>().text = "押された";
+    }
+}
---
+++ CR Sea Story/Assets/DOTween/UI_test/moving.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class moving : MonoBehaviour
+{
+    void Awake()
+    {
+        StartStrongButtonAnim();
+    }
+
+    void StartStrongButtonAnim()
+    {
+        transform.DOScale(0.1f, 1.0f)
+        .SetRelative(true)
+        .SetEase(Ease.OutQuart)
+        .SetLoops(-1, LoopType.Restart);
+    }
+}
