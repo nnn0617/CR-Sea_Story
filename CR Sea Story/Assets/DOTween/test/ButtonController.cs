7e5c9e56fbaf3d54aa5b1ed853d75c4c7909f721
nnn0617 <natu0617k@gmail.com>
2020-10-09 10:51:28 +0900

    stageSel更新

--- CR Sea Story/Assets/DOTween/test/ButtonController.cs
+++ CR Sea Story/Assets/DOTween/test/ButtonController.cs
@@ -3,36 +3,14 @@ using DG.Tweening;
 
 public class ButtonController : MonoBehaviour
 {
-    RectTransform rectTransform;
-
     void Awake()
     {
-        rectTransform = GetComponent<RectTransform>();
-        rectTransform.anchoredPosition = new Vector3(300, 700, 0);
+        transform.localScale = Vector3.zero;
         ShowWindow();
     }
 
     void ShowWindow()
     {
-        rectTransform.DOLocalMoveY(0f, 2f).SetEase(Ease.OutBounce);
+        transform.DOScale(1f, 1.5f).SetEase(Ease.OutBounce);
     }
 }
-
-
-
-//using UnityEngine;
-//using DG.Tweening;
-
-//public class ButtonController : MonoBehaviour
-//{
-//    void Awake()
-//    {
-//        transform.localScale = Vector3.zero;
-//        ShowWindow();
-//    }
-
-//    void ShowWindow()
-//    {
-//        transform.DOScale(1f, 1f).SetEase(Ease.OutBounce);
-//    }
-//}
---
+++ CR Sea Story/Assets/DOTween/test/ButtonController2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class ButtonController2 : MonoBehaviour
+{
+    RectTransform rectTransform;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector3(300, 700, 0);
+        ShowWindow();
+    }
+
+    void ShowWindow()
+    {
+        rectTransform.DOLocalMoveY(0f, 2f).SetEase(Ease.OutBounce);
+    }
+}
