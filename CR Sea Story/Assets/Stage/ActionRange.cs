cc3d2109428947ba2f618c33367af291c176d1cf
hidaka <shadow.konbu@icloud.com>
2020-10-15 16:17:20 +0900

    タイルマップ系

--- CR Sea Story/Assets/Stage/ActionRange.cs
+++ CR Sea Story/Assets/Stage/ActionRange.cs
@@ -8,21 +8,56 @@ public class ActionRange : MonoBehaviour, IPointerClickHandler
     [SerializeField] TileBase _passibleTile;
     [SerializeField] TileBase _attackbleTile;
 
-    //到達可能のマスの可視化
-    void ShowPassibleTile(Vector3Int pos, int maxStep)
+    private PlayerMove _playerMove;
+
+    void Start()
     {
-        
+        _playerMove = GameObject.Find("Player").GetComponent<PlayerMove>();
+    }
+
+    //到達可能のマスの可視化
+    void ShowPassibleTile(Vector3Int unitPos, int maxStep)
+    {
+        CheckPassible(unitPos, maxStep + 1);
     }
 
     //到達できるかのチェック
-    void CheckPossible(Vector3Int pos, int remainStep)
+    void CheckPassible(Vector3Int pos, int remainStep)
     {
-        
+        //if ()
+        //{
+
+        //}
+        _actionRange.SetTile(pos, _passibleTile);
+        --remainStep;
+        if (remainStep == 0) return;
+
+        CheckPassible(pos + Vector3Int.up, remainStep);
+        CheckPassible(pos + Vector3Int.left, remainStep);
+        CheckPassible(pos + Vector3Int.right, remainStep);
+        CheckPassible(pos + Vector3Int.down, remainStep);
     }
 
     //クリック時の処理
     public void OnPointerClick(PointerEventData pointerEventData)
     {
+        if (_playerMove.GetSelectionFlag)
+        {
+            TileBase clickedTile = _actionRange.GetTile(_playerMove.GetMouseClickPos);
+            if (clickedTile == _passibleTile)
+            {
 
+            }
+            if (clickedTile == _attackbleTile)
+            {
+
+            }
+            //else if (クリックした地点にまだ行動できるユニットがいる)
+            //{
+            //その地点にいるユニットを選択状態にする処理
+
+            //ShowPassibleTile(ユニットの地点、ユニットの移動可能な距離);
+            //}
+        }
     }
 }
