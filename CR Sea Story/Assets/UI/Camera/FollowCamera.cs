f30aa672f3d6cc767fbe4bd87054549d55077590
hidaka <shadow.konbu@icloud.com>
2020-10-16 11:32:05 +0900

    カメラ追従

---
+++ CR Sea Story/Assets/UI/Camera/FollowCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class FollowCamera : MonoBehaviour
+{
+    private Vector3 _playerPos;
+    float _deltaTime = 0.0f;
+
+    void LateUpdate()
+    {
+        _playerPos = GameObject.FindGameObjectWithTag("Player").gameObject.transform.position;
+        _playerPos.z = transform.position.z;
+        transform.position = Vector3.Lerp(transform.position, _playerPos, 0.5f * _deltaTime);
+        _deltaTime += Time.deltaTime;
+    }
+}
