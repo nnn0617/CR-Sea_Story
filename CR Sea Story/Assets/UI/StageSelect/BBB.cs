be6c17d417d1e0fa890af4fc09e20c0c662416fa
nnn0617 <natu0617k@gmail.com>
2020-11-30 15:24:26 +0900

    stageSel更新

---
+++ CR Sea Story/Assets/UI/StageSelect/BBB.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BBB : MonoBehaviour
+{
+    RectTransform rectTransform;
+    Sequence seq = DOTween.Sequence();
+
+    void Awake()
+    {
+        transform.localScale = Vector3.zero;
+        ShowWindow();
+    }
+
+    public void ShowWindow()
+    {
+        //　SetEase = アニメーション
+        seq.Append(transform.DOScale(5f, 1f).SetEase(Ease.OutBounce));
+    }
+}
