ddd0125044100348cdef41aa4fdc84afdaeb225d
Natuki_GoE <natu0617k@gmail.com>
2021-02-05 13:03:03 +0900

    a

---
+++ CR Sea Story/Assets/Scenes/Script/ReGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReGame : MonoBehaviour
+{
+    [SerializeField] private AudioSource ButtonSE;
+    void Start()
+    {
+        FadeManager.FadeIn();
+    }
+    bool isCalledOnce = false;
+
+    public void OnClickStartButton()
+    {
+        if (!isCalledOnce)
+        {
+            isCalledOnce = true;
+            FadeManager.FadeOut(4);
+            Debug.Log("GameSceneへ");
+        }
+    }
+}
